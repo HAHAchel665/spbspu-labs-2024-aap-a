786ac096a4ffab85210ff4e030aa71d71d9610f9
root <root@DESKTOP-KOVAQT8.localdomain>
2024-11-12 23:31:57 +0300

    add func to main

--- balashov.dima/P4/main.cpp
+++ balashov.dima/P4/main.cpp
@@ -1,4 +1,5 @@
-#include<iostream>
+#include <iostream>
+#include "processingString.hpp"
 
 int main()
 {
@@ -9,4 +10,24 @@ int main()
     std::cerr << "Memory was not allocated\n";
     return 1;
   }
+  try
+  {
+    line = balashov::enteringLine(capacity);
+  }
+    catch(const std::bad_alloc& e)
+  {
+    free(line);
+    std::cerr << "Memory was not allocated\n";
+    return 1;
+  }
+  if (line[0] == '\0')
+  {
+    free(line);
+    std::cerr << "The absence of a line\n";
+    return 1;
+  }
+  std::cout << balashov::removeSpaces(line) << "\n";
+  free(line);
+  return 0;
 }
+
